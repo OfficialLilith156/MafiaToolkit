5937191b75e9432c0c5811617569fd92562417e0
Official_Lilith <romanovanatasa58@gmail.com>
2025-12-18 19:57:06 +0300

    Update text version 2.46

--- Mafia2Libs/Utils/Settings/ToolkitSettings.cs
+++ Mafia2Libs/Utils/Settings/ToolkitSettings.cs
@@ -61,7 +61,7 @@ namespace Utils.Settings
 
         // Update vars
         public static float CurrentVersion = 1.0f;
-        public static readonly float Version = 2.45f;
+        public static readonly float Version = 2.46f;
 
         public static void ReadINI()
         {
