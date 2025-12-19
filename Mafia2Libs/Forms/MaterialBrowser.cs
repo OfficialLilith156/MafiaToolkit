95f738662791c410f1f2032c7f94f7f1096880a8
Official_Lilith <romanovanatasa58@gmail.com>
2025-12-19 23:29:30 +0300

    Cleaning up the MaterialBrowser.cs code

--- Mafia2Libs/Forms/MaterialBrowser.cs
+++ Mafia2Libs/Forms/MaterialBrowser.cs
@@ -30,12 +30,10 @@ namespace Mafia2Tool.Forms
                 var lib = MaterialsManager.MaterialLibraries.ElementAt(i).Value;
                 ComboBox_Materials.Items.Add(lib.Name);
             }
-
             ComboBox_Materials.SelectedIndex = 0;
             ComboBox_SearchType.SelectedIndex = 0;
             Label_MaterialCount.Text = "";
             TextBox_SearchBar.Text = materialName;
-
             PerformSearch();
         }
 
@@ -46,7 +44,6 @@ namespace Mafia2Tool.Forms
             Label_SelectMatLib.Text = Language.GetString("$LABEL_SELECTMATLIBRARY");
             Label_SearchType.Text = Language.GetString("$LABEL_SEARCHTYPE");
             Text = Language.GetString("$TITLE_MATERIALBROWSER");
-
             for (int i = 0; i < ComboBox_SearchType.Items.Count; i++)
             {
                 var text = (ComboBox_SearchType.Items[i] as string);
@@ -63,7 +60,6 @@ namespace Mafia2Tool.Forms
         private void PopulateBrowser(IMaterial[] materials)
         {
             FlowPanel_Materials.Controls.Clear();
-
             for (int x = 0; x < materials.Length; x++)
             {
                 var mat = materials[x];
@@ -78,7 +74,6 @@ namespace Mafia2Tool.Forms
         private void PerformSearch()
         {
             string text = TextBox_SearchBar.Text;
-
             // We should not search if the search bar is empty, or we'll get some terrible results!
             if (!string.IsNullOrEmpty(text))
             {
@@ -100,13 +95,11 @@ namespace Mafia2Tool.Forms
         {
             // Add the new selected one
             TextureEntry Entry = (sender as TextureEntry);
-
             // Remove the previous entry, if it is the same then we select this and exit the browser
             if (SelectedEntry != null)
             {
                 SelectedEntry.IsSelected = false;
             }
-
             SelectedEntry = Entry;
         }
 
